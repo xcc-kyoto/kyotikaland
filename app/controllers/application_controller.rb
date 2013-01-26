425d2f962caed9ec8a3f8341a850ca04632372b3
Yasuhiro Usutani <y_usutani@me.com>
2013-01-26 14:00:46 +0900

    Digest認証をかける
    ENV["BASIC_AUTH_NAME"]
    ENV["BASIC_AUTH_PW"]
    
    Cloud Foundry:
    vmc help --all | grep env
    vmc set-env APP NAME [VALUE]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,17 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  REALM = "a12CV4jsf2sArJs6Y"
+  before_filter :authenticate
+
+  private
+
+  def authenticate
+    name, pw = ENV["BASIC_AUTH_NAME"], ENV["BASIC_AUTH_PW"]
+    return if name.nil? || pw.nil?
+    users = { name => pw }
+    authenticate_or_request_with_http_digest(REALM) do |username|
+      users[username]
+    end
+  end
 end
