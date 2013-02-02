4570e5ddee8ddcc408e9f2a95b82e36f52a7ced2
Yasuhiro Usutani <y_usutani@me.com>
2013-02-02 20:32:13 +0900

    シードデータをタグに変更

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,4 @@ l = Landmark.create(name: "清水寺",
                     answer2: "藤原仲成",
                     answer3: "文室綿麻呂",
                     correct: 2)
-l.keywords.create([{body: "坂上田村麻呂"}, {body: "源氏物語"}, {body: "真言宗"}])
+l.tags.create([{name: "坂上田村麻呂"}, {name: "源氏物語"}, {name: "真言宗"}])
