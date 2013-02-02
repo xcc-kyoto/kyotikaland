3adc7406005aa4d7cea012dd1b2ac77a9a29b266
Yasuhiro Usutani <y_usutani@me.com>
2013-02-02 19:40:13 +0900

    タグビューの追加

--- app/helpers/landmarks_helper.rb
+++ app/helpers/landmarks_helper.rb
@@ -2,4 +2,8 @@ module LandmarksHelper
   def join_keywords(landmark)
     landmark.keywords.map { |k| k.body }.join(", ")
   end
+
+  def join_tags(landmark)
+    landmark.tags.map { |k| k.name }.join(", ")
+  end
 end
--- app/models/landmark.rb
+++ app/models/landmark.rb
@@ -1,7 +1,12 @@
 class Landmark < ActiveRecord::Base
-  attr_accessible :answer1, :answer2, :answer3, :correct, :latitude, :longitude, :name, :question, :url, :author
+  attr_accessible :answer1, :answer2, :answer3, :correct, :latitude, :longitude, :name, :question, :url, :author, :tag_tokens
   validates :correct, numericality: { greater_than_or_equal_to: 1, less_than_or_equal_to: 3 }
   has_many :keywords
   has_many :taggings
   has_many :tags, through: :taggings
+  attr_reader :tag_tokens
+
+  def tag_tokens=(tokens)
+    self.tag_ids = Tag.ids_from_tokens(tokens)
+  end
 end
